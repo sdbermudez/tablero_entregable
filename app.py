38484bffb5b87d800ab5cbf22e06c04361e7e045
sdbermudez <142225867+sdbermudez@users.noreply.github.com>
2025-04-09 12:25:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -123,5 +123,5 @@ def actualizar_fig_pais(orden):
 if __name__ == '__main__':
     #app.run_server(debug=True) 
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
 
